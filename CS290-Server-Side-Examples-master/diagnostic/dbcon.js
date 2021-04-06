246d071b01f410f54640b3e95473fc8e25426543
daykills <daykills@gmail.com>
2021-04-05 22:05:57 -0700

    Assignment 1
    modify dbcon.js

--- CS290-Server-Side-Examples-master/diagnostic/dbcon.js
+++ CS290-Server-Side-Examples-master/diagnostic/dbcon.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs290_yourengrusername',
-  password        : 'last-4-digits-of-your-osu-id',
-  database        : 'cs290_yourengrusername'
+  user            : 'cs290_chensij',
+  password        : '6547',
+  database        : 'cs290_chensij'
 });
 
 module.exports.pool = pool;
