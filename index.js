b34c529ac4b5a99670c983a2c259b05f8bcb038b
daykills <daykills@gmail.com>
2021-05-26 22:53:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const handlebars = require('express-handlebars').create({defaultLayout:'main'});
-const ip = "127.0.0.1"
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.use(express.json());
@@ -28,27 +27,6 @@ app.get('/travel', function(req, res){
   res.render('travel')
 });
 
-/*When the user visits the ladder subpage, the site automatically
-fetches live player ranking data from Path of Exile's API and outputs
-the rank, name, level, class, and online status info to the ladder
-handlebar*/
-app.get('/contacts', function(req, res){
-
-  var ladder_list = []
-  var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-  var request = new XMLHttpRequest();
-  
-  request.open("GET", "http://api.pathofexile.com/ladders/Standard?offset=0&limit=200", false);
-  request.send(null);
-
-  let data = JSON.parse(request.responseText);
-
-  var queryList = []
-  for (var i=0; i<199; i++){
-    queryList[queryList.length] = {'rank': i, 'name': data.entries[i].character.name, 
-    'level': data.entries[i].character.level, 'class': data.entries[i].character.class, 
-    'online': data.entries[i].online};
-  }
   var resObj = {queryObj: queryList};
   res.render('contacts', resObj);
 });
@@ -70,6 +48,6 @@ app.use(function(err, req, res, next){
 });
 
 app.listen(app.get('port'), function(){
-    console.log(`Express started on http://${ip}:${app.get('port')}`);
+    console.log(`Express started on http://${process.env.HOSTNAME}:${app.get('port')}`);
   });
 
